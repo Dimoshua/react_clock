5782737a6fa6619c9ffdf95159f418a5b7f3101a
Dmytro Moshkivskyi <dmosh@ukr.net>
2024-11-20 18:42:56 +0200

    react_clock by Dimoshua

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
-import './App.scss';
 import { Clock } from './Clock';
 
 type State = {
   clockName: string;
   hasClock: boolean;
 };
+
+function getRandomName(): string {
+  const value = Date.now().toString().slice(-4);
+
+  return `Clock-${value}`;
+}
 
 export class App extends React.Component {
   state: State = {
@@ -15,41 +20,31 @@ export class App extends React.Component {
 
   timerName = 0;
 
-  getRandomName(): string {
-    const value = Date.now().toString().slice(-4);
-
-    return `Clock-${value}`;
-  }
-
-  handleClockHide = (event: MouseEvent): void => {
+  hideClock = (event: MouseEvent): void => {
     event.preventDefault();
-    this.setState({
-      hasClock: false,
-    });
+    this.setState({ hasClock: false });
   };
 
-  handleClockShow = (): void => {
-    this.setState({
-      hasClock: true,
-    });
+  showClock = (): void => {
+    this.setState({ hasClock: true });
   };
 
   componentDidMount(): void {
     this.timerName = window.setInterval(() => {
-      this.setState({ clockName: this.getRandomName() });
+      this.setState({ clockName: getRandomName() });
     }, 3300);
     document.addEventListener('contextmenu', (event: MouseEvent) => {
-      this.handleClockHide(event);
+      this.hideClock(event);
     });
 
     document.addEventListener('click', () => {
-      this.handleClockShow();
+      this.showClock();
     });
   }
 
   componentWillUnmount(): void {
-    document.removeEventListener('contextmenu', this.handleClockHide);
-    document.removeEventListener('click', this.handleClockShow);
+    document.removeEventListener('contextmenu', this.hideClock);
+    document.removeEventListener('click', this.showClock);
     window.clearInterval(this.timerName);
   }
 
--- src/Clock.tsx
+++ src/Clock.tsx
@@ -5,40 +5,22 @@ type Props = {
   name: string;
 };
 
-type State = {
-  currentTime: string;
-};
-
-export class Clock extends React.Component<Props, State> {
-  state: State = {
-    currentTime: new Date().toUTCString().slice(-12, -4),
+// eslint-disable-next-line react/prefer-stateless-function
+export class Clock extends React.Component<Props> {
+  state = {
+    currentTime: new Date(),
   };
 
   timerTime = 0;
 
   componentDidMount(): void {
-    this.timerTime = window.setInterval(() => {
-      this.setState({
-        currentTime: new Date().toUTCString().slice(-12, -4),
-      });
-    }, 1000);
+    this.startClockUpdate();
   }
 
-  componentDidUpdate(
-    prevProps: Readonly<Props>,
-    prevState: Readonly<State>,
-  ): void {
-    const prevName = prevProps.name;
-    const currentName = this.props.name;
-
-    if (prevState.currentTime !== this.state.currentTime) {
+  componentDidUpdate(prevProps: Readonly<Props>): void {
+    if (prevProps.name !== this.props.name) {
       // eslint-disable-next-line no-console
-      console.log(this.state.currentTime);
-    }
-
-    if (prevName !== currentName) {
-      // eslint-disable-next-line no-console
-      console.warn(`Renamed from ${prevName} to ${currentName}`);
+      console.debug(`Renamed from ${prevProps.name} to ${this.props.name}`);
     }
   }
 
@@ -46,8 +28,17 @@ export class Clock extends React.Component<Props, State> {
     window.clearInterval(this.timerTime);
   }
 
+  startClockUpdate = () => {
+    this.timerTime = window.setInterval(() => {
+      this.setState({ today: new Date() });
+      // eslint-disable-next-line no-console
+      console.log(new Date().toUTCString().slice(-12, -4));
+    }, 1000);
+  };
+
   render() {
     const { name } = this.props;
+    const { currentTime } = this.state;
 
     return (
       <div className="Clock">
@@ -55,7 +46,9 @@ export class Clock extends React.Component<Props, State> {
 
         {' time is '}
 
-        <span className="Clock__time">{this.state.currentTime}</span>
+        <span className="Clock__time">
+          {currentTime.toUTCString().slice(-12, -4)}
+        </span>
       </div>
     );
   }
